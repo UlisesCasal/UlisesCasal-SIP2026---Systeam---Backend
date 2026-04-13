0fec4e99e3c6cfec2ecfb835990477a4cd90126f
Ulises Casal <83846014+UlisesCasal@users.noreply.github.com>
2026-04-13 09:30:05 -0300

    Fix de import

--- backend/src/main/java/com/systeam/backend/UserAdministration/model/Role.java
+++ backend/src/main/java/com/systeam/backend/UserAdministration/model/Role.java
@@ -5,7 +5,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 import org.hibernate.annotations.CreationTimestamp;
-
+import jakarta.persistence.*;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
--- backend/src/main/java/com/systeam/backend/UserAdministration/model/User.java
+++ backend/src/main/java/com/systeam/backend/UserAdministration/model/User.java
@@ -6,7 +6,7 @@ import java.util.Set;
 
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
-
+import jakarta.persistence.*;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
